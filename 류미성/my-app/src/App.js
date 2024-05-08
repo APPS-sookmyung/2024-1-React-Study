f2af410045e70fc5b93e97bf09876fd136fafbc6
misung.dev <misung.dev@gmail.com>
2024-05-08 16:42:37 +0900

    Feat: jsx 문법 사용해보기

---
+++ 류미성/my-app/src/App.js
@@ -0,0 +1,26 @@
+import "./App.css";
+
+function App() {
+	const name = "앨리";
+	const list = ["우유", "딸기", "바나나"];
+
+	return (
+		<>
+			<h1> {`Hello! ${name}`}</h1>
+			<h2>Hello!</h2>
+			<p>{name}</p>
+			<ul>
+				{/* Arrow function */}
+				{list.map((item) => (
+					<li>{item}</li>
+				))}
+				{/* function */}
+				{list.map(function (item) {
+					return <li>{item}</li>;
+				})}
+			</ul>
+		</>
+	);
+}
+
+export default App;
