bfc9a55a23d85ef0218e5f7e1bbb1c2e32f7a6b0
misung.dev <misung.dev@gmail.com>
2024-05-08 19:32:12 +0900

    Feat: 카운터 상태 끌어 올리기

--- 류미성/my-app/src/AppCounter.jsx
+++ 류미성/my-app/src/AppCounter.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import "./App.css";
 import Counter from "./components/Counter";
+import { useState } from "react";
 
 const AppCounter = () => {
+	const [count, setCount] = useState(0);
+	const handleClick = () => setCount((prev) => prev + 1);
+
 	return (
-		<div>
-			<Counter />
+		<div className="container">
+			<div className="banner">
+				Total Count: {count} {count > 10 ? "🔥" : "❄️"}
+			</div>
+			<div className="counters">
+				<Counter total={count} onClick={handleClick} />
+				<Counter total={count} onClick={handleClick} />
+			</div>
 		</div>
 	);
 };
--- 류미성/my-app/src/components/Counter.jsx
+++ 류미성/my-app/src/components/Counter.jsx
@@ -1,26 +1,18 @@
 import React, { useState } from "react";
 
-const Counter = () => {
+const Counter = ({ total, onClick }) => {
 	const [count, setCount] = useState(0);
 
 	return (
 		<div className="counter">
-			<span className="number">{count}</span>
+			<p className="number">
+				{count} <span className="total">/{total}</span>
+			</p>
 			<button
 				className="button"
 				onClick={() => {
-					// count++는 const로 지정했으므로, 재할당은 불가하므로 setCount를 사용하기
-					// setCount(count + 1);
-					// setCount(count + 1);
-					// setCount(count + 1);
-					// setCount(count + 1);
-					// setCount(count + 1); // 1에서 더이상 숫자가 증가하지 않음
-					// 콜백함수 형태로 set하는 것이 더 안전함
 					setCount((prev) => prev + 1);
-					setCount((prev) => prev + 1);
-					setCount((prev) => prev + 1);
-					setCount((prev) => prev + 1);
-					setCount((prev) => prev + 1); // 5단위로 증가
+					onClick();
 				}}
 			>
 				Add +
