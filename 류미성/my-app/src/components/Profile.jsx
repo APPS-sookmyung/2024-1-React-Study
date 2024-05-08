d77c618c0a793ad2424b5a95829dc20c93632339
misung.dev <misung.dev@gmail.com>
2024-05-08 18:33:42 +0900

    Feat: 새로운 개발자 태그 만들기

--- 류미성/my-app/src/components/Profile.jsx
+++ 류미성/my-app/src/components/Profile.jsx
@@ -1,7 +1,9 @@
-export default function Profile({ image, name, title }) {
+export default function Profile({ image, name, title, isNew }) {
 	return (
 		<div className="profile">
 			<img className="photo" src={image} alt="프로필 이미지" />
+			{/* isNew가 true인 경우만 해당 태그 보이도록 지정 */}
+			{isNew && <span className="new">New</span>}
 			<h1>{name}</h1>
 			<p>{title}</p>
 		</div>
