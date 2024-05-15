0d42f1fca8f38dbbb0eac1569edd11f9a2ca0c32
seoy-54 <semi0091@naver.com>
2024-05-15 21:30:10 +0900

    Feat: 4주차 필기 및 코드

---
+++ seoyeon/basic2/src/basic/AppJSX.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+
+function AppJSX() {
+  const name = "서연";
+  const list = ["우유", "딸기", "바나나"];
+  return (
+    <>
+      <h1 className="orange">{`Hello! ${name}`}</h1>
+      <h2>Hello!</h2>
+      <p>{name}</p>
+      <ul>
+        {list.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
+      <img
+        style={{ width: "200px", height: "200px" }}
+        src="https://images.unsplash.com/photo-1714997219940-6d9cd56c3609?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="nature"
+      />
+    </>
+  );
+}
+
+export default AppJSX;
